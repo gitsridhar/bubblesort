6d721b752d6b147401a9aff2e7c8783d803a7ee3
Sridhar Venkat <svenkat@macbook-pro-3.raleigh.ibm.com>
2019-05-16 14:02:50 -0400

    Added optimized implementation

---
+++ bubblesortoptimized.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void swap(int *left, int *right) {
+    int temp = *left;
+
+    *left = *right;
+    *right = temp;
+}
+
+void bubblesort(int data[], int n) {
+    bool alreadyprocessed;
+    for(int i=0; i<n-1; i++) {
+        alreadyprocessed = false;
+        for(int j=0; j<n-i-1; j++) {
+            if(data[j] > data[j+1]) {
+                swap(&data[j], &data[j+1]);
+                alreadyprocessed = true;
+            }
+        }
+        if (alreadyprocessed == false) {
+            break;
+        }
+    }
+}
+
+int main() {
+    int data[] = {1,9,2,8,3,7,4,6,5};
+    bubblesort(data, sizeof(data)/sizeof(data[0]));
+
+    for (int i=0; i<sizeof(data)/sizeof(data[0]); i++) {
+        printf("%d ", data[i]);
+    }
+
+    return 0;
+}
